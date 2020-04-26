e488f20784e6edfed3b25b576968fc6942c1196d
algos912 <44305112+algos912@users.noreply.github.com>
2020-04-25 21:00:40 -0700

    Create 15_Insert_into_a_Cyclic_Sorted_List.java

---
+++ Linked_List/15_Insert_into_a_Cyclic_Sorted_List.java
@@ -0,0 +1,3 @@
+// https://leetcode.com/problems/insert-into-a-cyclic-sorted-list/
+// Given a node from a cyclic linked list which is sorted in ascending order, write a function to insert a value into the list such that it remains a cyclic sorted list. The given node can be a reference to any single node in the list, and may not be necessarily the smallest value in the cyclic list.
+
