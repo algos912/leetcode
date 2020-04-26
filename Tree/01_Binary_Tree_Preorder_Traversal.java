c8feb9e273066e71dadf3aea55e9379706fe2e9f
algos912 <44305112+algos912@users.noreply.github.com>
2020-04-26 11:53:11 -0700

    Create 01_Binary_Tree_Preorder_Traversal.java

---
+++ Tree/01_Binary_Tree_Preorder_Traversal.java
@@ -0,0 +1,41 @@
+// https://leetcode.com/explore/learn/card/data-structure-tree/134/traverse-a-tree/928/
+// Given a binary tree, return the preorder traversal of its nodes' values.
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+ 
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> output = new ArrayList<Integer>();
+        
+        // check for invalid inputs
+        if(root == null)
+            return output;
+        
+        // add the nodes of three into a stack
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        
+        //preorder = root, left, right
+        while(!stack.empty()){
+            TreeNode n = stack.pop();
+            output.add(n.val);
+            
+            if(n.right != null)
+                stack.push(n.right);
+            
+            if(n.left != null)
+                stack.push(n.left);
+            
+        }
+        return output;
+        
+    }
+}
