334d9f454cc468ae9570609dc711f93486a4ef7f
algos912 <44305112+algos912@users.noreply.github.com>
2020-04-27 13:53:39 -0700

    Update 02_Binary_Tree_Inorder_Traversal.java

--- Tree/02_Binary_Tree_Inorder_Traversal.java
+++ Tree/02_Binary_Tree_Inorder_Traversal.java
@@ -11,6 +11,7 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+/*
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> output = new ArrayList<Integer>();
@@ -43,5 +44,25 @@ class Solution {
         // return the output list
         return output;
         
+    }
+}
+*/
+
+// RECURSIVE SOLUTION
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> output = new ArrayList<Integer>();
+        helper(root, output);
+        return output;   
+    }
+    
+    public void helper(TreeNode root, List<Integer> output) {
+        if(root != null) {
+            if(root.left != null)
+                helper(root.left, output);
+            output.add(root.val);
+            if(root.right != null)
+                helper(root.right, output);
+        }
     }
 }
