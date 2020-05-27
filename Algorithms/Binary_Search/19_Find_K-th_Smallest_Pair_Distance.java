36f51b5e5d46becfaa2c28bc84e8cd459df88373
algos912 <44305112+algos912@users.noreply.github.com>
2020-05-27 11:30:20 -0700

    Update 19_Find_K-th_Smallest_Pair_Distance.java

--- Algorithms/Binary_Search/19_Find_K-th_Smallest_Pair_Distance.java
+++ Algorithms/Binary_Search/19_Find_K-th_Smallest_Pair_Distance.java
@@ -2,6 +2,10 @@
 // Given an integer array, return the k-th smallest distance among all the pairs. 
 // The distance of a pair (A, B) is defined as the absolute difference between A and B.
 
+// TC : nlog(n) (for Java comparison based sorting) + nlog(n) (for binary search with countPairs in O(n))
+// TC : nlog(n)
+// SC : O(1)
+
 class Solution {
     public int smallestDistancePair(int[] nums, int k) {
         
