e560552fa27a5db1191c666b4ec96e8ec443e33b
algos912 <44305112+algos912@users.noreply.github.com>
2020-05-29 08:30:58 -0700

    Create Queue.java

---
+++ Queue/Queue.java
@@ -0,0 +1,60 @@
+// https://leetcode.com/explore/learn/card/queue-stack/228/first-in-first-out-data-structure/1366/
+/*
+Implement a Queue (FIFO)
+To implement a queue, we may use a dynamic array and an index pointing to the head of the queue.
+As mentioned, a queue should support two operations: enqueue and dequeue. 
+Enqueue appends a new element to the queue while dequeue removes the first element. So we need an index to indicate the starting point.
+/*
+
+// "static void main" must be defined in a public class.
+
+class MyQueue {
+    // store elements
+    private List<Integer> data;         
+    // a pointer to indicate the start position
+    private int p_start;            
+    public MyQueue() {
+        data = new ArrayList<Integer>();
+        p_start = 0;
+    }
+    /** Insert an element into the queue. Return true if the operation is successful. */
+    public boolean enQueue(int x) {
+        data.add(x);
+        return true;
+    };    
+    /** Delete an element from the queue. Return true if the operation is successful. */
+    public boolean deQueue() {
+        if (isEmpty() == true) {
+            return false;
+        }
+        p_start++;
+        return true;
+    }
+    /** Get the front item from the queue. */
+    public int Front() {
+        return data.get(p_start);
+    }
+    /** Checks whether the queue is empty or not. */
+    public boolean isEmpty() {
+        return p_start >= data.size();
+    }     
+};
+
+public class Main {
+    public static void main(String[] args) {
+        MyQueue q = new MyQueue();
+        q.enQueue(5);
+        q.enQueue(3);
+        if (q.isEmpty() == false) {
+            System.out.println(q.Front());
+        }
+        q.deQueue();
+        if (q.isEmpty() == false) {
+            System.out.println(q.Front());
+        }
+        q.deQueue();
+        if (q.isEmpty() == false) {
+            System.out.println(q.Front());
+        }
+    }
+}
