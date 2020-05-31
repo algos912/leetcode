c71941d682e982d4edf8a16ec6c2b7d2381993a7
algos912 <44305112+algos912@users.noreply.github.com>
2020-05-30 20:44:50 -0700

    Update 07_Perfect_Squares_BFS.java

--- Queue/07_Perfect_Squares_BFS.java
+++ Queue/07_Perfect_Squares_BFS.java
@@ -1,4 +1,5 @@
 // https://leetcode.com/explore/learn/card/queue-stack/231/practical-application-queue/1371/
+// https://leetcode.com/problems/perfect-squares/discuss/165688/BFS-and-DP
 // Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
 
 class Solution {
@@ -41,3 +42,22 @@ class Solution {
         return -1;
     }
 }
+
+/*
+public int numSquares01(int n) {
+
+        int[] state = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            state[i] = i;
+            for (int j = 1; j <= i; j++) {
+                if (j * j > i) {
+                    break;
+                }
+                state[i] = Math.min(state[i - j * j] + 1, state[i]);
+            }
+        }
+
+        return state[n];
+  }
+    
+*/
