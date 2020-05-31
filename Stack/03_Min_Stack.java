e6c489aabd50c1acdd7d9f7415b2424e2b113b1a
algos912 <44305112+algos912@users.noreply.github.com>
2020-05-30 17:38:21 -0700

    Update 03_Min_Stack.java

--- Stack/03_Min_Stack.java
+++ Stack/03_Min_Stack.java
@@ -46,3 +46,104 @@ class MinStack {
  * int param_3 = obj.top();
  * int param_4 = obj.getMin();
  */
+
+// SOLUTION - 2 : USING TWO STACKS
+/*
+class MinStack {
+    
+    private Stack<Integer> stack;
+    private Stack<Integer> min_stack;
+
+    // initialize your data structure here.
+    public MinStack() {
+        this.stack=new Stack();
+        this.min_stack=new Stack();
+    }
+    
+    public void push(int x) {
+        if(min_stack.isEmpty() || x<=min_stack.peek()){
+            min_stack.push(x);
+        }
+        stack.push(x);
+    }
+    
+    public void pop() 
+    {
+        if(stack.peek().equals(min_stack.peek())){
+            min_stack.pop();
+        }
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return min_stack.peek();
+    }
+}
+*/
+
+// SOLUTION -3 : WITHOUT USING IN-BUILT STACK
+/*
+class MinStack {
+	Node head;
+	int min;
+	// initialize your data structure here.
+	public MinStack() {
+	    head=null;
+	    min=999999;
+	}
+
+	public void push(int x) {
+		if(head==null){
+			head=new Node(x);
+			min=x;
+			Node temp=new Node(min);
+			temp.next=head;
+			head=temp;
+			return;
+		}
+		if(x<min)
+			min=x;
+		Node temp=new Node(min);
+		temp.next=head;
+		Node temp2=new Node(x);
+		temp2.next=temp;
+		head=temp2;
+
+	}
+
+	public void pop() {
+		Node temp=head.next.next;
+		head=head.next.next;
+		if(temp==null)min=999999;
+		else
+		min=temp.next.data;
+	}
+
+	public int top() {
+		return head.data;
+	}
+
+	public int getMin() {
+		return head.next.data;
+	}
+}
+class Node{
+	int data;
+	Node next;
+	public Node(int data){
+		this.data=data;
+	}
+}
+*/
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
