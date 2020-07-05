b52cf3866224d4b57d64315865d34136db0ebeaf
algos912 <44305112+algos912@users.noreply.github.com>
2020-07-05 15:16:12 -0700

    Create 19_Find_Duplicate_Subtrees.java

---
+++ Hash Table/19_Find_Duplicate_Subtrees.java
@@ -0,0 +1,67 @@
+// https://leetcode.com/explore/learn/card/hash-table/185/hash_table_design_the_key/1127/
+// https://leetcode.com/problems/find-duplicate-subtrees/solution/
+/*
+Given a binary tree, return all duplicate subtrees. For each kind of duplicate subtrees, you only need to return the root node of any one of them.
+
+Two trees are duplicate if they have the same structure with same node values.
+
+Example 1:
+
+        1
+       / \
+      2   3
+     /   / \
+    4   2   4
+       /
+      4
+The following are two duplicate subtrees:
+
+      2
+     /
+    4
+and
+
+    4
+Therefore, you need to return above trees' root in the form of a list.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int t;
+    Map<String, Integer> trees;
+    Map<Integer, Integer> count;
+    List<TreeNode> ans;
+
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        t = 1;
+        trees = new HashMap();
+        count = new HashMap();
+        ans = new ArrayList();
+        lookup(root);
+        return ans;
+    }
+
+    public int lookup(TreeNode node) {
+        if (node == null) return 0;
+        String serial = node.val + "," + lookup(node.left) + "," + lookup(node.right);
+        int uid = trees.computeIfAbsent(serial, x-> t++);
+        count.put(uid, count.getOrDefault(uid, 0) + 1);
+        if (count.get(uid) == 2)
+            ans.add(node);
+        return uid;
+    }
+}
