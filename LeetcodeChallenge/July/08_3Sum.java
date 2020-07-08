85ce329cdeaee64d36fb5c68a924a656aa80a445
algos912 <44305112+algos912@users.noreply.github.com>
2020-07-08 16:27:13 -0700

    Create 08_3Sum.java

---
+++ LeetcodeChallenge/July/08_3Sum.java
@@ -0,0 +1,42 @@
+// https://leetcode.com/explore/challenge/card/july-leetcoding-challenge/545/week-2-july-8th-july-14th/3384/
+// https://leetcode.com/problems/3sum/solution/
+/*
+Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+
+Note:
+
+The solution set must not contain duplicate triplets.
+
+Example:
+
+Given array nums = [-1, 0, 1, 2, -1, -4],
+
+A solution set is:
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+*/
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        
+        List<List<Integer>> res = new ArrayList<>();
+        Set<Pair> found = new HashSet<>();
+        for (int i = 0; i < nums.length; ++i) {
+        Set<Integer> seen = new HashSet<>();
+        for (int j = i + 1; j < nums.length; ++j) {
+            int complement = -nums[i] - nums[j];
+            if (seen.contains(complement)) {
+                int v1 = Math.min(nums[i], Math.min(complement, nums[j]));
+                int v2 = Math.max(nums[i], Math.max(complement, nums[j]));
+                if (found.add(new Pair(v1, v2)))
+                    res.add(Arrays.asList(nums[i], complement, nums[j]));
+            }
+            seen.add(nums[j]);
+        }
+        }
+        return res;
+        
+    }
+}
