c8d8bee718d8b004a3caaa98c5e6f59709cf61df
algos912 <44305112+algos912@users.noreply.github.com>
2020-07-08 14:35:53 -0700

    Create 02_Swap_Nodes_in_Pairs.java

---
+++ Algorithms/Recursion/02_Swap_Nodes_in_Pairs.java
@@ -0,0 +1,46 @@
+// https://leetcode.com/explore/learn/card/recursion-i/250/principle-of-recursion/1681/
+// https://leetcode.com/problems/swap-nodes-in-pairs/solution/
+/*
+Given a linked list, swap every two adjacent nodes and return its head.
+
+You may not modify the values in the list's nodes, only nodes itself may be changed.
+
+ 
+
+Example:
+
+Given 1->2->3->4, you should return the list as 2->1->4->3.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+ 
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        
+        // If the list has no node or has only one node left.
+        if ((head == null) || (head.next == null)) {
+            return head;
+        }
+
+        // Nodes to be swapped
+        ListNode firstNode = head;
+        ListNode secondNode = head.next;
+
+        // Swapping
+        firstNode.next  = swapPairs(secondNode.next);
+        secondNode.next = firstNode;
+
+        // Now the head is the second node
+        return secondNode;
+        
+    }
+}
