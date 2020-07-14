6227f5df99938607dfae2208b91cb27709a20543
algos912 <44305112+algos912@users.noreply.github.com>
2020-07-14 11:09:36 -0700

    Update 12_Search_a_2D_Matrix_II.java

--- Algorithms/Recursion/12_Search_a_2D_Matrix_II.java
+++ Algorithms/Recursion/12_Search_a_2D_Matrix_II.java
@@ -1,4 +1,5 @@
 // https://leetcode.com/explore/learn/card/recursion-ii/470/divide-and-conquer/2872/
+// https://www.programcreek.com/2014/04/leetcode-search-a-2d-matrix-ii-java/
 /*
 Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
 
@@ -26,6 +27,7 @@ class Solution {
         // SOLUTION : 1  BRUTE FORCE
         // TC : O(nm)
         // SC : O(1)
+        /*
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
                 if (matrix[i][j] == target) {
@@ -34,6 +36,30 @@ class Solution {
             }
         }
         return false;
+        */
         
+        // SOLUTION 2 : BINARY SEARCH
+        // TC : O(n+m)
+        
+        // check for invalid inputs
+        if(matrix.length == 0 || matrix[0].length == 0)
+            return false;
+        
+        int row = matrix.length-1;
+        int col = matrix[0].length-1;
+ 
+        int i = row; 
+        int j = 0;
+ 
+        while(i >= 0 && j <= col){
+            if(target < matrix[i][j]){
+                i--;
+            }else if(target > matrix[i][j]){
+                j++;
+            }else{
+                return true;
+            }
+        }
+        return false;
     }
 }
