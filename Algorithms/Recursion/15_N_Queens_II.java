dcae6368e0ccb23b8fdee9f953bf0e9d125f84f0
algos912 <44305112+algos912@users.noreply.github.com>
2020-07-14 12:16:55 -0700

    Create 15_N_Queens_II.java

---
+++ Algorithms/Recursion/15_N_Queens_II.java
@@ -0,0 +1,73 @@
+// https://leetcode.com/problems/n-queens-ii/
+// https://leetcode.com/explore/learn/card/recursion-ii/472/backtracking/2804/
+/*
+The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.
+
+Given an integer n, return the number of distinct solutions to the n-queens puzzle.
+
+Example:
+
+Input: 4
+Output: 2
+Explanation: There are two distinct solutions to the 4-queens puzzle as shown below.
+[
+ [".Q..",  // Solution 1
+  "...Q",
+  "Q...",
+  "..Q."],
+
+ ["..Q.",  // Solution 2
+  "Q...",
+  "...Q",
+  ".Q.."]
+]
+*/
+
+// TC : O(N!)
+// SC : O(N)
+
+class Solution {
+  public boolean is_not_under_attack(int row, int col, int n,
+                                     int [] rows,
+                                     int [] hills,
+                                     int [] dales) {
+    int res = rows[col] + hills[row - col + 2 * n] + dales[row + col];
+    return (res == 0) ? true : false;
+  }
+
+  public int backtrack(int row, int count, int n,
+                       int [] rows,
+                       int [] hills,
+                       int [] dales) {
+    for (int col = 0; col < n; col++) {
+      if (is_not_under_attack(row, col, n, rows, hills, dales)) {
+        // place_queen
+        rows[col] = 1;
+        hills[row - col + 2 * n] = 1;  // "hill" diagonals
+        dales[row + col] = 1;   //"dale" diagonals    
+
+        // if n queens are already placed
+        if (row + 1 == n) count++;
+        // if not proceed to place the rest
+        else count = backtrack(row + 1, count, n,
+                rows, hills, dales);
+
+        // remove queen
+        rows[col] = 0;
+        hills[row - col + 2 * n] = 0;
+        dales[row + col] = 0;
+      }
+    }
+    return count;
+  }
+
+  public int totalNQueens(int n) {
+    int rows[] = new int[n];
+    // "hill" diagonals
+    int hills[] = new int[4 * n - 1];
+    // "dale" diagonals
+    int dales[] = new int[2 * n - 1];
+
+    return backtrack(0, 0, n, rows, hills, dales);
+  }
+}
