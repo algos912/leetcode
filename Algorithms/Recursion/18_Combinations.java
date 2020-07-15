1b46eae8605bdc9e3e407c3c6de235bbdbfbb883
algos912 <44305112+algos912@users.noreply.github.com>
2020-07-15 10:05:49 -0700

    Create 18_Combinations.java

---
+++ Algorithms/Recursion/18_Combinations.java
@@ -0,0 +1,46 @@
+// https://leetcode.com/explore/learn/card/recursion-ii/472/backtracking/2798/
+// https://leetcode.com/problems/combinations/solution/
+/*
+Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+
+Example:
+
+Input: n = 4, k = 2
+Output:
+[
+  [2,4],
+  [3,4],
+  [2,3],
+  [1,2],
+  [1,3],
+  [1,4],
+]
+*/
+
+class Solution {
+  List<List<Integer>> output = new LinkedList();
+  int n;
+  int k;
+
+  public void backtrack(int first, LinkedList<Integer> curr) {
+    // if the combination is done
+    if (curr.size() == k)
+      output.add(new LinkedList(curr));
+
+    for (int i = first; i < n + 1; ++i) {
+      // add i into the current combination
+      curr.add(i);
+      // use next integers to complete the combination
+      backtrack(i + 1, curr);
+      // backtrack
+      curr.removeLast();
+    }
+  }
+
+  public List<List<Integer>> combine(int n, int k) {
+    this.n = n;
+    this.k = k;
+    backtrack(1, new LinkedList<Integer>());
+    return output;
+  }
+}
