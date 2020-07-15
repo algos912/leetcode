63d14f4d7256434c8fd2a08d56c8f5ce85a03e9a
algos912 <44305112+algos912@users.noreply.github.com>
2020-07-15 10:16:09 -0700

    Create 19_SameTree.java

---
+++ Algorithms/Recursion/19_SameTree.java
@@ -0,0 +1,62 @@
+// https://leetcode.com/explore/learn/card/recursion-ii/503/recursion-to-iteration/2894/
+// Given two binary trees, write a function to check if they are the same or not.
+// Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+
+// RECURSION
+class Solution {
+  public boolean isSameTree(TreeNode p, TreeNode q) {
+    // p and q are both null
+    if (p == null && q == null) return true;
+    // one of p and q is null
+    if (q == null || p == null) return false;
+    if (p.val != q.val) return false;
+    return isSameTree(p.right, q.right) &&
+            isSameTree(p.left, q.left);
+  }
+}
+
+// https://leetcode.com/explore/learn/card/recursion-ii/503/recursion-to-iteration/2693/
+// ITERATION
+/*
+class Solution {
+  public boolean check(TreeNode p, TreeNode q) {
+    // p and q are null
+    if (p == null && q == null) return true;
+    // one of p and q is null
+    if (q == null || p == null) return false;
+    if (p.val != q.val) return false;
+    return true;
+  }
+
+  public boolean isSameTree(TreeNode p, TreeNode q) {
+    if (p == null && q == null) return true;
+    if (!check(p, q)) return false;
+    // init deques
+    ArrayDeque<TreeNode> deqP = new ArrayDeque<TreeNode>();
+    ArrayDeque<TreeNode> deqQ = new ArrayDeque<TreeNode>();
+    deqP.addLast(p);
+    deqQ.addLast(q);
+
+    while (!deqP.isEmpty()) {
+      p = deqP.removeFirst();
+      q = deqQ.removeFirst();
+
+      if (!check(p, q)) return false;
+      if (p != null) {
+        // in Java nulls are not allowed in Deque
+        if (!check(p.left, q.left)) return false;
+        if (p.left != null) {
+          deqP.addLast(p.left);
+          deqQ.addLast(q.left);
+        }
+        if (!check(p.right, q.right)) return false;
+        if (p.right != null) {
+          deqP.addLast(p.right);
+          deqQ.addLast(q.right);
+        }
+      }
+    }
+    return true;
+  }
+}
+*/
