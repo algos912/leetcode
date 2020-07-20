9d551b3d657a878e52941ddeaed3446fafb7cccc
algos912 <44305112+algos912@users.noreply.github.com>
2020-07-20 15:28:48 -0700

    Create 09_Contains_Duplicate_III.java

---
+++ Tree/BST/09_Contains_Duplicate_III.java
@@ -0,0 +1,60 @@
+// https://leetcode.com/articles/contains-duplicate-iii/
+/*
+Given an array of integers, find out whether there are two distinct indices i and j in the array such that the absolute difference between nums[i] and nums[j] is at most t and the absolute difference between i and j is at most k.
+
+Example 1:
+Input: nums = [1,2,3,1], k = 3, t = 0
+Output: true
+
+Example 2:
+Input: nums = [1,0,1,1], k = 1, t = 2
+Output: true
+
+Example 3:
+Input: nums = [1,5,9,1,5,9], k = 2, t = 3
+Output: false
+*/
+
+// https://www.programcreek.com/2014/06/leetcode-contains-duplicate-iii-java/
+
+class Solution {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        
+        // BRUTE FORCE
+        /*
+        // Overflow : Wrong edge case
+        for (int i = 0; i < nums.length; ++i) {
+            for (int j = Math.max(i - k, 0); j < i; ++j) {
+                if (Math.abs(nums[i] - nums[j]) <= t) return true;
+            }
+        }
+        return false;
+        */
+        
+        
+        // Using TreeSet
+        // TC : O(nlogk)
+        if(nums == null || nums.length < 2 || k < 0 || t < 0)
+            return false;
+ 
+        TreeSet<Long> set = new TreeSet<Long>();
+        for(int i = 0; i < nums.length; i++){
+            long curr = (long) nums[i];
+ 
+            long leftBoundary = (long) curr-t;
+            long rightBoundary = (long) curr+t+1; //right boundary is exclusive, so +1
+            SortedSet<Long> sub = set.subSet(leftBoundary, rightBoundary);
+            if(sub.size() > 0)
+                return true;
+ 
+            set.add(curr);   
+ 
+            if(i >= k){ // or if(set.size()>=k+1)
+                set.remove((long)nums[i-k]);
+            }
+        }
+ 
+        return false;
+        
+    }
+}
