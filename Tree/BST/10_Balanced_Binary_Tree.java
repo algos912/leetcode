7cbd7734c916efe2cbce0699b28e5be0bea708d7
algos912 <44305112+algos912@users.noreply.github.com>
2020-07-20 16:10:23 -0700

    Create 10_Balanced_Binary_Tree.java

---
+++ Tree/BST/10_Balanced_Binary_Tree.java
@@ -0,0 +1,51 @@
+// https://leetcode.com/explore/learn/card/introduction-to-data-structure-binary-search-tree/143/appendix-height-balanced-bst/1027/
+// https://leetcode.com/problems/balanced-binary-tree/solution/
+/*
+Given a binary tree, determine if it is height-balanced.
+
+For this problem, a height-balanced binary tree is defined as:
+a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+ 
+ // TC : O(nlogn)
+ // SC : O(n)
+ 
+class Solution {
+  // Recursively obtain the height of a tree. An empty tree has -1 height
+  private int height(TreeNode root) {
+    // An empty tree has height -1
+    if (root == null) {
+      return -1;
+    }
+    return 1 + Math.max(height(root.left), height(root.right));
+  }
+
+  public boolean isBalanced(TreeNode root) {
+    // An empty tree satisfies the definition of a balanced tree
+    if (root == null) {
+      return true;
+    }
+
+    // Check if subtrees have height within 1. If they do, check if the
+    // subtrees are balanced
+    return Math.abs(height(root.left) - height(root.right)) < 2
+        && isBalanced(root.left)
+        && isBalanced(root.right);
+  }
+};
