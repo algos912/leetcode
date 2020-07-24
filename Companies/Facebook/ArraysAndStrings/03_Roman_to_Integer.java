0b4389972ed6bfac0c3d8ac0f4f714303f75fc73
algos912 <44305112+algos912@users.noreply.github.com>
2020-07-24 14:35:38 -0700

    Create 03_Roman_to_Integer.java

---
+++ Companies/Facebook/ArraysAndStrings/03_Roman_to_Integer.java
@@ -0,0 +1,73 @@
+// https://leetcode.com/explore/interview/card/facebook/5/array-and-strings/3010/
+/*
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, two is written as II in Roman numeral, just two one's added together. Twelve is written as, XII, which is simply X + II. The number twenty seven is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer. Input is guaranteed to be within the range from 1 to 3999.
+
+Example 1:
+
+Input: "III"
+Output: 3
+Example 2:
+
+Input: "IV"
+Output: 4
+
+Rules:
+* If I comes before V or X, subtract 1 eg: IV = 4 and IX = 9
+* If X comes before L or C, subtract 10 eg: XL = 40 and XC = 90
+* If C comes before D or M, subtract 100 eg: CD = 400 and CM = 900
+*/
+
+// https://leetcode.com/problems/roman-to-integer/solution/
+// Right to left Pass
+// TC : O(1)
+// SC : O(1)
+
+class Solution {
+    
+    static Map<String, Integer> values = new HashMap<>();
+    
+    static {
+        values.put("M", 1000);
+        values.put("D", 500);
+        values.put("C", 100);
+        values.put("L", 50);
+        values.put("X", 10);
+        values.put("V", 5);
+        values.put("I", 1);
+    }
+
+    public int romanToInt(String s) {
+        
+        String lastSymbol = s.substring(s.length() - 1);
+        int lastValue = values.get(lastSymbol);
+        int total = lastValue;
+
+        for (int i = s.length() - 2; i >= 0; i--) {
+            String currentSymbol = s.substring(i, i + 1);
+            int currentValue = values.get(currentSymbol);
+            if (currentValue < lastValue) {
+                total -= currentValue;
+            } else {
+                total += currentValue;
+            }
+            lastValue = currentValue;
+        }
+        return total;
+    }
+}
