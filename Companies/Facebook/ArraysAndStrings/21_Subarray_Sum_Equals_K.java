098881ad8b0bee06ceb95200b81d16956eb70c43
algos912 <44305112+algos912@users.noreply.github.com>
2020-07-27 22:04:52 -0700

    Create 21_Subarray_Sum_Equals_K.java

---
+++ Companies/Facebook/ArraysAndStrings/21_Subarray_Sum_Equals_K.java
@@ -0,0 +1,36 @@
+// Subarray Sum Equals K
+// https://leetcode.com/explore/interview/card/facebook/5/array-and-strings/3019/
+/*
+Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
+
+Example 1:
+Input:nums = [1,1,1], k = 2
+Output: 2
+ 
+Constraints:
+The length of the array is in range [1, 20,000].
+The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].
+*/
+
+// https://leetcode.com/problems/subarray-sum-equals-k/discuss/102106/Java-Solution-PreSum-%2B-HashMap
+// brute force, TC : O(n2) , SC : O(1)
+// TC : O(n)
+// SC : O(n)
+
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int sum = 0, result = 0;
+        Map<Integer, Integer> preSum = new HashMap<>();
+        preSum.put(0, 1);
+        
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (preSum.containsKey(sum - k)) {
+                result += preSum.get(sum - k);
+            }
+            preSum.put(sum, preSum.getOrDefault(sum, 0) + 1);
+        }
+        
+        return result;
+    }
+}
