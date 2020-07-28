ca55f9a823ff696298e8b3db3ecab2f4e609eeae
algos912 <44305112+algos912@users.noreply.github.com>
2020-07-27 21:31:12 -0700

    Update 16_Product_of_Array_Except_Self.java

--- Companies/Facebook/ArraysAndStrings/16_Product_of_Array_Except_Self.java
+++ Companies/Facebook/ArraysAndStrings/16_Product_of_Array_Except_Self.java
@@ -16,19 +16,23 @@ Could you solve it with constant space complexity? (The output array does not co
 
 // https://leetcode.com/problems/product-of-array-except-self/discuss/65622/Simple-Java-solution-in-O(n)-without-extra-space
 
-class Solution {
+public class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
         int[] res = new int[n];
-        res[0] = 1;
-        for (int i = 1; i < n; i++) {
-            res[i] = res[i - 1] * nums[i - 1];
+        // Calculate lefts and store in res.
+        int left = 1;
+        for (int i = 0; i < n; i++) {
+            if (i > 0)
+                left = left * nums[i - 1];
+            res[i] = left;
         }
-        
+        // Calculate rights and the product from the end of the array.
         int right = 1;
         for (int i = n - 1; i >= 0; i--) {
+            if (i < n - 1)
+                right = right * nums[i + 1];
             res[i] *= right;
-            right *= nums[i];
         }
         return res;
     }
