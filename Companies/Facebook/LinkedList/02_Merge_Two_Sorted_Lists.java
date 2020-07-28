42737c094d4a5d1cb4bdcff2a3e037cfc0fc74b1
algos912 <44305112+algos912@users.noreply.github.com>
2020-07-28 15:23:09 -0700

    Create 02_Merge_Two_Sorted_Lists.java

---
+++ Companies/Facebook/LinkedList/02_Merge_Two_Sorted_Lists.java
@@ -0,0 +1,53 @@
+// Merge Two Sorted Lists
+// https://leetcode.com/explore/interview/card/facebook/6/linked-list/301/
+/*
+Merge two sorted linked lists and return it as a new sorted list. The new list should be made by splicing together the nodes of the first two lists.
+
+Example:
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        
+        //define a new fake head
+        ListNode head = new ListNode(0);
+        ListNode p = head;
+        
+        //compare first elements from each list and add smaller one to merged list
+        ListNode p1 = l1;
+        ListNode p2 = l2;
+        while(p1 != null && p2 != null) {
+            if(p1.val < p2.val) {
+                p.next = p1;
+                p1 = p1.next;
+            } else {
+                p.next = p2;
+                p2 = p2.next;
+            }
+            p = p.next;
+        }
+        
+        //when one of the lists are empty, just append the other list to merged-list
+        if(p1 != null) {
+            p.next = p1;
+        }
+        
+        if(p2 != null) {
+            p.next = p2;
+        }
+        
+        //return head of merged list
+        return head.next;
+        
+    }
+}
