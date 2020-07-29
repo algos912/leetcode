ebec1517f72a3afafb055ecdf28e9fe9c491e9fe
algos912 <44305112+algos912@users.noreply.github.com>
2020-07-29 15:08:38 -0700

    Create 08_Binary_Tree_Paths.java

---
+++ Companies/Facebook/Tree/08_Binary_Tree_Paths.java
@@ -0,0 +1,55 @@
+// Binary Tree Paths
+// https://leetcode.com/explore/interview/card/facebook/52/trees-and-graphs/280/
+/*
+Given a binary tree, return all root-to-leaf paths.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Input:
+
+   1
+ /   \
+2     3
+ \
+  5
+
+Output: ["1->2->5", "1->3"]
+
+Explanation: All root-to-leaf paths are: 1->2->5, 1->3
+*/
+
+// https://leetcode.com/problems/binary-tree-paths/discuss/68258/Accepted-Java-simple-solution-in-8-lines
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> answer = new ArrayList<String>();
+        if (root != null) 
+            searchBT(root, "", answer);
+        return answer;
+    }
+    private void searchBT(TreeNode root, String path, List<String> answer) {
+        if (root.left == null && root.right == null) 
+            answer.add(path + root.val);
+        
+        if (root.left != null) 
+            searchBT(root.left, path + root.val + "->", answer);
+        if (root.right != null) 
+            searchBT(root.right, path + root.val + "->", answer);
+    }
+}
