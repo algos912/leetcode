d25ac424b9a8095274649f0d0230d621e274ad39
algos912 <44305112+algos912@users.noreply.github.com>
2020-08-31 11:13:01 -0700

    Create 04_Permutation_in_String.java

---
+++ Companies/Facebook/Algorithms/Others/04_Permutation_in_String.java
@@ -0,0 +1,43 @@
+// Permutation in String
+/*
+Given two strings s1 and s2, write a function to return true if s2 contains the permutation of s1. In other words, one of the first string's permutations is the substring of the second string.
+
+ 
+
+Example 1:
+
+Input: s1 = "ab" s2 = "eidbaooo"
+Output: True
+Explanation: s2 contains one permutation of s1 ("ba").
+Example 2:
+
+Input:s1= "ab" s2 = "eidboaoo"
+Output: False
+ 
+
+Constraints:
+
+The input strings only contain lower case letters.
+The length of both given strings is in range [1, 10,000].
+*/
+
+// https://leetcode.com/problems/permutation-in-string/solution/
+// sorting
+// TC : O(l1)
+// SC : O(l1)
+
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        s1 = sort(s1);
+        for (int i = 0; i <= s2.length() - s1.length(); i++) {
+            if (s1.equals(sort(s2.substring(i, i + s1.length()))))
+                return true;
+        }
+        return false;
+    }
+    public String sort(String s) {
+        char[] t = s.toCharArray();
+        Arrays.sort(t);
+        return new String(t);
+    }
+}
