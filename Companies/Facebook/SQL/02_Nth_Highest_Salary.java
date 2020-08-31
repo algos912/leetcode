708f4cd44aeabb2ec1ab94e1d55c214af4651856
algos912 <44305112+algos912@users.noreply.github.com>
2020-08-31 12:44:08 -0700

    Create 02_Nth_Highest_Salary.java

---
+++ Companies/Facebook/SQL/02_Nth_Highest_Salary.java
@@ -0,0 +1,37 @@
+/*
+Nth Highest Salary
+
+Write a SQL query to get the nth highest salary from the Employee table.
+
++----+--------+
+| Id | Salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+For example, given the above Employee table, the nth highest salary where n = 2 is 200. If there is no nth highest salary, then the query should return null.
+
++------------------------+
+| getNthHighestSalary(2) |
++------------------------+
+| 200                    |
++------------------------+
+
+Solution:
+https://leetcode.com/problems/nth-highest-salary/discuss/185368/Fast-and-easy-to-understand-mysql-solution
+*/
+
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  SET N=N-1;
+  RETURN (
+      # Write your MySQL query statement below.
+      SELECT Salary
+      FROM Employee
+      GROUP BY Salary
+      ORDER BY Salary DESC
+      LIMIT 1 OFFSET N
+      
+  );
+END
