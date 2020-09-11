56b3bb7e2049ba8d5761fd9cba4d2cfcc78e27fd
algos912 <44305112+algos912@users.noreply.github.com>
2020-09-11 11:42:47 -0700

    Create 02_Container_With_Most_Water.java

---
+++ Companies/Google/ArraysAndStrings/Arrays/02_Container_With_Most_Water.java
@@ -0,0 +1,51 @@
+// Container With Most Water
+// https://leetcode.com/explore/interview/card/google/59/array-and-strings/3048/
+/*
+Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+
+Note: You may not slant the container and n is at least 2.
+
+ 
+
+
+
+The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+
+ 
+
+Example:
+
+Input: [1,8,6,2,5,4,8,3,7]
+Output: 49
+
+*/
+
+class Solution {
+    public int maxArea(int[] height) {
+        
+        // Brute force
+        // TC : O(n2)
+        // SC : O(1)
+        /*
+        int maxarea = 0;
+        for (int i = 0; i < height.length; i++)
+            for (int j = i + 1; j < height.length; j++)
+                maxarea = Math.max(maxarea, Math.min(height[i], height[j]) * (j - i));
+        return maxarea;
+        */
+        
+        // two pointer
+        // TC : O(n) single pass
+        // SC : O(1)
+        int maxarea = 0, l = 0, r = height.length - 1;
+        while (l < r) {
+            maxarea = Math.max(maxarea, Math.min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r])
+                l++;
+            else
+                r--;
+        }
+        return maxarea;
+        
+    }
+}
