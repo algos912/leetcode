3fe560bc57536d4c3df6df727aea1aded73e8fb6
algos912 <44305112+algos912@users.noreply.github.com>
2020-12-28 19:48:25 -0800

    Update 17_Rotate_Array.java

--- Arrays and Strings/17_Rotate_Array.java
+++ Arrays and Strings/17_Rotate_Array.java
@@ -4,85 +4,41 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         
-        int len = nums.length;
-        //check for invalid inputs
-        if( len <= 1 || k == 0)
+        if(nums == null || nums.length == 0 || k == 0)
             return;
         
-        //if k is greater than input array length
-        if(k > len)
-            k = k - len;
-        
-        // 1. Solution using additional memory
-        // using an external array for storage
-        // Space : O(n)
-        // Time : O(n)
-        
-        int[] output = new int[len];
-        int index = 0;
-        for(int i = 0; i < len; i++) {
-            if(i+k < len) {
-                index = i + k;    
-            } else {
-                index = i + k - len;
-            } 
-            output[index] = nums[i];
-        }
-        
-        // Copy output array back to input array
+        // Solution 1 : use additional memory
+        // TC : O(n)
+        // SC : O(n)
+        /*
+        int[] buffer = new int[nums.length];
         for(int i = 0; i < nums.length; i++) {
-            nums[i] = output[i];
+            buffer[(i+k) % nums.length] = nums[i];
         }
         
-        // Could you do it in-place with O(1) extra space?
-        // 2. Bubble rotate (like bubble sort)
-        // Space : O(1)
-        // Time : O(n*k)
-        
-        /*
-        for(int i = 0; i < k; i++) {
-            for(int j = len-1; j > 0; j--) {
-                int tmp = nums[j];
-                nums[j] = nums[j-1];
-                nums[j-1] = tmp;
-            }
+        for(int i = 0; i < nums.length; i++) {
+            nums[i] = buffer[i];
         }
         */
         
-        // 3. Use Array Reverse
-        // Space : O(1)
-        // Time : O(n)
-        
-        // Assuming we are given {1,2,3,4,5,6} and order 2. The basic idea is:
-        // 1. Divide the array two parts: 1,2,3,4 and 5, 6
-        // 2. Reverse first part: 4,3,2,1,5,6
-        // 3. Reverse second part: 4,3,2,1,6,5
-        // 4. Reverse the whole array: 5,6,1,2,3,4
-         
-        /*
-        //find length of first part
-        int firstPartLength = len - k;
-        
-        reverse(nums, 0, firstPartLength-1);
-        reverse(nums, firstPartLength, len-1);
-        reverse(nums, 0, len-1);
-        */
+        // Solution 2 : in-place
+        // TC : O(n)
+        // SC : O(1)
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
         
     }
     
     //reverse array in-place
-    public void reverse(int[] arr, int start, int end) {
-        
-        if(arr == null || arr.length == 1) 
-            return;
-        
-        while (start < end) {
-            int tmp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = tmp;
-            
-            start++;
-            end--;
-        }
+    public void reverse(int[] nums, int start, int end) {
+    while (start < end) {
+      int temp = nums[start];
+      nums[start] = nums[end];
+      nums[end] = temp;
+      start++;
+      end--;
     }
+  }
 }
