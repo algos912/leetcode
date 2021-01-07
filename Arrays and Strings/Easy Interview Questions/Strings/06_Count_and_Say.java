bddead868347c2ca36fedc4abc8d4a7a06c0f09a
algos912 <44305112+algos912@users.noreply.github.com>
2021-01-06 17:12:03 -0800

    Update 06_Count_and_Say.java

--- Arrays and Strings/Easy Interview Questions/Strings/06_Count_and_Say.java
+++ Arrays and Strings/Easy Interview Questions/Strings/06_Count_and_Say.java
@@ -59,17 +59,17 @@ class Solution {
         for (int i = 1; i < n; i++) {
             StringBuilder sb = new StringBuilder();
             int count = 1;
-            char ch = base.charAt(0);
+            char c = base.charAt(0);
             for (int j = 1; j < base.length(); j++) {
-                if (base.charAt(j) != ch) {
-                    sb.append(count).append(ch);
+                if (base.charAt(j) != c) {
+                    sb.append(count).append(c);
                     count = 1;
-                    ch = base.charAt(j);
+                    c = base.charAt(j);
                 } else {
                     count++;
                 }
             }
-            sb.append(count).append(ch);
+            sb.append(count).append(c);
             base = sb.toString();
         }
         return base;
