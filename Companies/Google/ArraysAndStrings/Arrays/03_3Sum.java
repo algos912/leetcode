ceabb6f5b63f9e4a92603b37998e91d1fbfee9bf
algos912 <44305112+algos912@users.noreply.github.com>
2021-01-15 14:13:07 -0800

    Update 03_3Sum.java

--- Companies/Google/ArraysAndStrings/Arrays/03_3Sum.java
+++ Companies/Google/ArraysAndStrings/Arrays/03_3Sum.java
@@ -27,26 +27,40 @@ Constraints:
 
 */
 
+// two pointers
+// TC : O(n2)
+// SC : O(n)
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        
-        List<List<Integer>> res = new ArrayList<>();
-        Set<Pair> found = new HashSet<>();
-        
-        for (int i = 0; i < nums.length; ++i) {
-            Set<Integer> seen = new HashSet<>();
-            for (int j = i + 1; j < nums.length; ++j) {
-                int complement = -nums[i] - nums[j];
-                if (seen.contains(complement)) {
-                    int v1 = Math.min(nums[i], Math.min(complement, nums[j]));
-                    int v2 = Math.max(nums[i], Math.max(complement, nums[j]));
-                    if (found.add(new Pair(v1, v2)))
-                        res.add(Arrays.asList(nums[i], complement, nums[j]));
-            }
-            seen.add(nums[j]);
+        Arrays.sort(nums);
+        List<List<Integer>> output = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++) {
+            if(i == 0 || nums[i-1] != nums[i])
+                twoSum(nums, i , output);
+        }
+        return output;  
+    }
+    
+    public void twoSum(int[] nums, int i, List<List<Integer>> output) {
+        int low = i+1;
+        int high = nums.length - 1;
+        int sum = 0;
+        while(low < high) {
+            sum = nums[i] + nums[low] + nums[high];
+            if(sum < 0) {
+                low++;
+            } else if(sum > 0) {
+                high--;
+            } else {
+                //threeSumList.add(nums[i]);
+                //threeSumList.add(nums[low++]);
+                //threeSumList.add(nums[high--]);
+                //output.add(threeSumList);
+                output.add(Arrays.asList(nums[i], nums[low++], nums[high--]));
+                while(low < high && nums[low] == nums[low-1])
+                    low++;
             }
         }
-        return res;
-        
     }
 }
