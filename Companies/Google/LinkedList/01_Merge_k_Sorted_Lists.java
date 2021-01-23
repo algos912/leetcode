1c63f309d724c0e76ea85f7de663b77a55406bb4
algos912 <44305112+algos912@users.noreply.github.com>
2021-01-22 16:00:46 -0800

    Create 01_Merge_k_Sorted_Lists.java

---
+++ Companies/Google/LinkedList/01_Merge_k_Sorted_Lists.java
@@ -0,0 +1,82 @@
+// Merge k Sorted Lists
+/*
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+Merge all the linked-lists into one sorted linked-list and return it.
+
+ 
+
+Example 1:
+
+Input: lists = [[1,4,5],[1,3,4],[2,6]]
+Output: [1,1,2,3,4,4,5,6]
+Explanation: The linked-lists are:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+merging them into one sorted list:
+1->1->2->3->4->4->5->6
+Example 2:
+
+Input: lists = []
+Output: []
+Example 3:
+
+Input: lists = [[]]
+Output: []
+ 
+
+Constraints:
+
+k == lists.length
+0 <= k <= 10^4
+0 <= lists[i].length <= 500
+-10^4 <= lists[i][j] <= 10^4
+lists[i] is sorted in ascending order.
+The sum of lists[i].length won't exceed 10^4.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+// https://leetcode.com/problems/merge-k-sorted-lists/discuss/152022/Brute-Force-or-Merge-Sort-or-Heap
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        
+        // check for invalid inputs
+        if(lists == null || lists.length == 0)
+            return null;
+        
+        ListNode merged = null;
+        for(ListNode head : lists) {
+            merged = mergeTwoLists(merged, head);
+        }
+        return merged;
+        
+    }
+    
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null)
+            return l2;
+        if(l2 == null)
+            return l1;
+        
+        if(l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+}
